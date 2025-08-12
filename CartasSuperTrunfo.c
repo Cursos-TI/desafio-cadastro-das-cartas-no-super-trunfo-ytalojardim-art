9215d50780fe14d576a89dfd0539cc1d0cc982e8
ytalojardim-art <ytalojardim@gmail.com>
2025-08-12 22:50:35 +0000

    identação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 
-int main() {
-    #include <stdio.h>
 
 int main() {                                 // variaveis para a carta numero 1
     char estado[85] ;  //usei [] pois o nome sera uma string , o numero 85 dentro é o maximo de caracteres que ira registrar essa variavel e a maior estado tem 85 letras
@@ -25,29 +23,29 @@ int main() {                                 // variaveis para a carta numero 1
 
 
 
-    printf("A seguir vamos começar o cadastro de cartas do super trunfo!\n");  // mensagem de anuncio de inicio de cadstro de cartas do super trunfo
+        printf("A seguir vamos começar o cadastro de cartas do super trunfo!\n");  // mensagem de anuncio de inicio de cadstro de cartas do super trunfo
 
 
-    printf("Digite o estado\n");// campo para preencher o nome do estado    
-    scanf("%s",&estado);  // codigo para registrar o campo preenchido
+        printf("Digite o estado\n");// campo para preencher o nome do estado    
+        scanf("%s",&estado);  // codigo para registrar o campo preenchido
     
-    printf("Digite o código da carta\n"); // campo para preencher o codigo da carta
-    scanf("%s",&carta);// codigo para registrar o campo preenchido
+        printf("Digite o código da carta\n"); // campo para preencher o codigo da carta
+        scanf("%s",&carta);// codigo para registrar o campo preenchido
 
-    printf("Digite o nome da cidade\n");// campo para preencher o nome da cidade
-    scanf("%s",&cidade);// codigo para registrar o campo preenchido
+        printf("Digite o nome da cidade\n");// campo para preencher o nome da cidade
+        scanf("%s",&cidade);// codigo para registrar o campo preenchido
 
-    printf("Digite a população da cidade\n");   // campo para preencher a população da cidade
-    scanf("%d",&população);// codigo para registrar o campo preenchido
+        printf("Digite a população da cidade\n");   // campo para preencher a população da cidade
+        scanf("%d",&população);// codigo para registrar o campo preenchido
 
-    printf("Digite a área da cidade em km²\n");// campo para preencher a area da cidade em km²
-    scanf("%f",&area);// codigo para registrar o campo preenchido
+        printf("Digite a área da cidade em km²\n");// campo para preencher a area da cidade em km²
+        scanf("%f",&area);// codigo para registrar o campo preenchido
 
-    printf("Digite o PIB da cidade\n");// campo para preencher o PIB da cidade
-    scanf("%f",&PIB);// codigo para registrar o campo preenchido
+        printf("Digite o PIB da cidade\n");// campo para preencher o PIB da cidade
+        scanf("%f",&PIB);// codigo para registrar o campo preenchido
 
-    printf("Digite a quantidade de pontos turisticos da cidade\n"); // campo para preencher pontos turisticos da cidade
-    scanf("%d",&turisticos);// codigo para registrar o campo preenchido
+        printf("Digite a quantidade de pontos turisticos da cidade\n"); // campo para preencher pontos turisticos da cidade
+        scanf("%d",&turisticos);// codigo para registrar o campo preenchido
 
     printf("carta 1\n");                        // comando para mostrar as informações registradas da carta numero 1
     printf("estado: %s\n", estado);
@@ -58,30 +56,32 @@ int main() {                                 // variaveis para a carta numero 1
     printf("PIB: %.2f bilhões de reais\n", PIB);
     printf("Número de Pontos Turísticos: %d\n", turisticos);
 
-    printf("Vamos para o cadastro da segunda carta!\n"); //mensagem de aviso que ira começar o cadastro da segunda carta
+    
+    
+        printf("Vamos para o cadastro da segunda carta!\n"); //mensagem de aviso que ira começar o cadastro da segunda carta
 
 
    
-    printf("Digite o estado\n");
-    scanf("%s",&estado2);// codigo para registrar o campo preenchido
+        printf("Digite o estado\n");
+        scanf("%s",&estado2);// codigo para registrar o campo preenchido
     
-    printf("Digite o código da carta\n");
-    scanf("%s",&carta2);// codigo para registrar o campo preenchido
+        printf("Digite o código da carta\n");
+        scanf("%s",&carta2);// codigo para registrar o campo preenchido
 
-    printf("Digite o nome da cidade\n");
-    scanf("%s",&cidade2);// codigo para registrar o campo preenchido
+        printf("Digite o nome da cidade\n");
+        scanf("%s",&cidade2);// codigo para registrar o campo preenchido
 
-    printf("Digite a população da cidade\n");   
-    scanf("%d",&população2);// codigo para registrar o campo preenchido
+        printf("Digite a população da cidade\n");   
+        scanf("%d",&população2);// codigo para registrar o campo preenchido
 
-    printf("Digite a área da cidade em km²\n");
-    scanf("%f",&area2);// codigo para registrar o campo preenchido
+        printf("Digite a área da cidade em km²\n");
+        scanf("%f",&area2);// codigo para registrar o campo preenchido
 
-    printf("Digite o PIB da cidade\n");
-    scanf("%f",&PIB2);// codigo para registrar o campo preenchido
+        printf("Digite o PIB da cidade\n");
+        scanf("%f",&PIB2);// codigo para registrar o campo preenchido
 
-    printf("Digite a quantidade de pontos turisticos da cidade\n");
-    scanf("%d",&turisticos2);// codigo para registrar o campo preenchido
+        printf("Digite a quantidade de pontos turisticos da cidade\n");
+        scanf("%d",&turisticos2);// codigo para registrar o campo preenchido
 
 
     
@@ -107,5 +107,4 @@ int main() {                                 // variaveis para a carta numero 1
 }
    
 
-    return 0;
-}
+
